8047545274bfce79dde1f80bc85287524cecbb0d
kn-shimada <xianrendaotian012@gmail.com>
2026-04-11 11:26:10 +0900

    Add: some features that missed to commit in last one

--- common/functions.py
+++ common/functions.py
@@ -13,8 +13,18 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
+
 def relu(x):
     return np.maximum(0, x)
+
+
+def relu_grad(x):
+    grad = np.zeros_like(x)
+    grad[x >= 1] = 1
+    return grad
 
 
 def softmax(x):
@@ -36,3 +46,8 @@ def cross_entropy_error(y, t):
 
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+def softmax_loss(X, t):
+    y = softmax(X)
+    return cross_entropy_error(y, t)
